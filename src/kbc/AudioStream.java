c5306380ee7bb84ca3cb33e729303e304c128101
Sanidhya Singh <ssanidhya620@gmail.com>
2024-06-26 12:10:13 +0530

    Add files via upload

---
+++ src/kbc/AudioStream.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kbc;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author Sanidhya Singh
+ */
+class AudioStream {
+
+    AudioStream(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/kbc/KBC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kbc;
+
+/**
+ *
+ * @author Sanidhya Singh
+ */
+public class KBC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
